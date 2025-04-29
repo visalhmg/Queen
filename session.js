97422f567bf9b3257f8ec0c7fc32b2fc7f6c3665
visalhmg <66855760+visalhmg@users.noreply.github.com>
2025-04-29 13:21:07 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~uA1wxboJ#fA_sPQBf4hEscUJLy67HEHdrUW8JMBsd4vChjZa3QkQ",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
